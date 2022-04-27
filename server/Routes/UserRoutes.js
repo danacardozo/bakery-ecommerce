be3ce9f81606312e665fb105d42619ba088ff44f
danacardozo <dana.n.cardozo@gmail.com>
2022-04-27 01:44:16 -0300

    profile API

--- server/Routes/UserRoutes.js
+++ server/Routes/UserRoutes.js
@@ -34,20 +34,7 @@ UserRoutes.post(
     "/profile", 
     protect,
     asyncHandler(async(req,res) => {
-        const user = await User.findById(req.user._id)
-
-        if (user) {
-            res.json({
-                _id: user._id,
-                name: user.name,
-                email: user.email,
-                isAdmin: user.isAdmin,
-                createdAt: user.createdAt,
-            })
-        } else {
-            res.status(404);
-        throw new Error("User not found");
-        }
+       res.send("user profile")
     })
     );
 
