401fb6d8fd43bdc7b404252770803913e5fc484e
danacardozo <dana.n.cardozo@gmail.com>
2022-04-27 01:57:10 -0300

    profile API

--- server/Middleware/AuthMiddleware.js
+++ server/Middleware/AuthMiddleware.js
@@ -11,19 +11,7 @@ const protect = asyncHandler(
             req.headers.authorization.startsWith("Bearer")
         )
         {
-           try {
-               token = req.headers.authorization.split(" ")[1]
-
-               const decoded = jwt.verify(token,process.env.JWT_SECRET)
-
-               req.user = await User.findById(decoded.id).select("-password");
-               next();
-
-           } catch (error){
-               console.error(error);
-               res.status(401)
-               throw new Error("Not authorized, token failed")
-           }
+           console.log("token found")
         }
         if (!token){
             res.status(401)
