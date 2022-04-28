b25d670e84c248b280c99220aa2482df75175f40
danacardozo <dana.n.cardozo@gmail.com>
2022-04-28 18:20:12 -0300

    logout screen

--- frontend/src/Redux/Actions/userActions.js
+++ frontend/src/Redux/Actions/userActions.js
@@ -1,5 +1,11 @@
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../Constants/UserConstants";
+import { 
+   USER_DETAILS_RESET, 
+   USER_LOGIN_FAIL, 
+   USER_LOGIN_REQUEST, 
+   USER_LOGIN_SUCCESS, 
+   USER_LOGOUT } from "../Constants/UserConstants";
 import axios from "axios";
+import { ORDER_LIST_MY_RESET } from "../Constants/OrderConstants";
 
 // iniciar sesion
 export const login = (email, password) => async (dispatch) => {
@@ -29,4 +35,13 @@ export const login = (email, password) => async (dispatch) => {
             : error.message,
       });
     }
+  };
+
+  //cerrar sesion
+  export const logout = () => (dispatch) => {
+    localStorage.removeItem("userInfo");
+    dispatch({ type: USER_LOGOUT });
+    dispatch({ type: USER_DETAILS_RESET });
+    dispatch({ type: ORDER_LIST_MY_RESET });
+    document.location.href = "/login";
   };
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,31 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link,useHistory } from "react-router-dom";
+import { logout } from "../Redux/Actions/userActions";
 
 const Header = () => {
   
+  const [keyword, setKeyword] = useState();
+  const dispatch = useDispatch();
+  let history = useHistory();
+
   const cart = useSelector((state) => state.cart);
-  const { cartItems} = cart;
+  const { cartItems } = cart;
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (keyword.trim()) {
+      history.push(`/search/${keyword}`);
+    } else {
+      history.push("/");
+    }
+  };
   return (
     <div>
       {/* Header */}
@@ -21,37 +41,66 @@ const Header = () => {
                   </Link>
                 </div>
                 <div className="col-6 d-flex align-items-center justify-content-end Login-Register">
-                  <div className="btn-group">
-                    <button
-                      type="button"
-                      className="name-button dropdown-toggle"
-                      data-toggle="dropdown"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      <i class="fas fa-user"></i>
-                    </button>
-                    <div className="dropdown-menu">
-                      <Link className="dropdown-item" to="/profile">
-                        Profile
-                      </Link>
+                  {userInfo ? (
+                    <div className="btn-group">
+                      <button
+                        type="button"
+                        className="name-button dropdown-toggle"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                      >
+                        <i class="fas fa-user"></i>
+                      </button>
+                      <div className="dropdown-menu">
+                        <Link className="dropdown-item" to="/profile">
+                          Profile
+                        </Link>
 
-                      <Link className="dropdown-item" to="#">
-                        Logout
-                      </Link>
+                        <Link
+                          className="dropdown-item"
+                          to="#"
+                          onClick={logoutHandler}
+                        >
+                          Logout
+                        </Link>
+                      </div>
                     </div>
-                  </div>
+                  ) : (
+                    <div className="btn-group">
+                      <button
+                        type="button"
+                        className="name-button dropdown-toggle"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
+                      >
+                        <i class="fas fa-user"></i>
+                      </button>
+                      <div className="dropdown-menu">
+                        <Link className="dropdown-item" to="/login">
+                          Login
+                        </Link>
+
+                        <Link className="dropdown-item" to="/register">
+                          Register
+                        </Link>
+                      </div>
+                    </div>
+                  )}
+
                   <Link to="/cart" className="cart-mobile-icon">
                     <i className="fas fa-shopping-bag"></i>
                     <span className="badge">{cartItems.length}</span>
                   </Link>
                 </div>
                 <div className="col-12 d-flex align-items-center">
-                  <form className="input-group">
+                  <form onSubmit={submitHandler} className="input-group">
                     <input
                       type="search"
                       className="form-control rounded search"
                       placeholder="Search"
+                      onChange={(e) => setKeyword(e.target.value)}
                     />
                     <button type="submit" className="search-button">
                       search
@@ -71,11 +120,12 @@ const Header = () => {
                 </Link>
               </div>
               <div className="col-md-6 col-8 d-flex align-items-center">
-                <form className="input-group">
+                <form onSubmit={submitHandler} className="input-group">
                   <input
                     type="search"
                     className="form-control rounded search"
                     placeholder="Search"
+                    onChange={(e) => setKeyword(e.target.value)}
                   />
                   <button type="submit" className="search-button">
                     search
@@ -83,26 +133,37 @@ const Header = () => {
                 </form>
               </div>
               <div className="col-md-3 d-flex align-items-center justify-content-end Login-Register">
-                <div className="btn-group">
-                  <button
-                    type="button"
-                    className="name-button dropdown-toggle"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    Hi, Random Admin
-                  </button>
-                  <div className="dropdown-menu">
-                    <Link className="dropdown-item" to="/profile">
-                      Profile
-                    </Link>
+                {userInfo ? (
+                  <div className="btn-group">
+                    <button
+                      type="button"
+                      className="name-button dropdown-toggle"
+                      data-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      Hi, {userInfo.name}
+                    </button>
+                    <div className="dropdown-menu">
+                      <Link className="dropdown-item" to="/profile">
+                        Profile
+                      </Link>
 
-                    <Link className="dropdown-item" to="#">
-                      Logout
-                    </Link>
+                      <Link
+                        className="dropdown-item"
+                        to="#"
+                        onClick={logoutHandler}
+                      >
+                        Logout
+                      </Link>
+                    </div>
                   </div>
-                </div>
+                ) : (
+                  <>
+                    <Link to="/register">Register</Link>
+                    <Link to="/login">Login</Link>
+                  </>
+                )}
 
                 <Link to="/cart">
                   <i className="fas fa-shopping-bag"></i>
