17bc925d89fc95a1003d4a01e16a9179434cd9ac
danacardozo <dana.n.cardozo@gmail.com>
2022-04-28 19:25:45 -0300

    update user profile API

--- server/Routes/UserRoutes.js
+++ server/Routes/UserRoutes.js
@@ -86,4 +86,33 @@ UserRoutes.post(
 })
 );
 
+//actualizar perfil:333
+UserRoutes.put(
+  "/profile",
+  protect,
+  asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      if (req.body.password) {
+        user.password = req.body.password;
+      }
+      const updatedUser = await user.save();
+      res.json({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        createdAt: updatedUser.createdAt,
+        token: generateToken(updatedUser._id),
+      });
+    } else {
+      res.status(404);
+      throw new Error("User not found");
+    }
+  })
+);
+
 export default UserRoutes;
