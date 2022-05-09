4cf8220f127d264c833c2324448942f3b6339c0f
danacardozo <dana.n.cardozo@gmail.com>
2022-05-09 01:45:39 -0300

    payment screen

--- frontend/src/Redux/Reducers/CartReducers.js
+++ frontend/src/Redux/Reducers/CartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from "../Constants/CartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from "../Constants/CartConstants";
 
 
 export const cartReducer = (state = { cartItems: [], shippingAddress: {}  }, action) => {
@@ -26,10 +26,15 @@ export const cartReducer = (state = { cartItems: [], shippingAddress: {}  }, act
                 cartItems: state.cartItems.filter((x) => x.product !== action.payload),
             };
             case CART_SAVE_SHIPPING_ADDRESS:
-      return {
-        ...state,
-        shippingAddress: action.payload,
+             return {
+               ...state,
+              shippingAddress: action.payload,
       };
+      case CART_SAVE_PAYMENT_METHOD:
+        return {
+          ...state,
+         paymentMethod: action.payload,
+ };
             default:
                 return state;
     }
