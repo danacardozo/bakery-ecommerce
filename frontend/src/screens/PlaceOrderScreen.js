9f81f61c92482cbf229e417be1187483546a31f5
danacardozo <dana.n.cardozo@gmail.com>
2022-05-09 03:54:33 -0300

    place an order screen

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import Message from "../components/LoadingError/Error";
 import Header from "./../components/Header";
 
 const PlaceOrderScreen = () => {
   window.scrollTo(0, 0);
+
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const userLogin = useSelector((state) => state.userLogin);
+  const {userInfo} = userLogin;
+
+  //calcular precio
+  const addDecimals = (num) => {
+    return (Math.round(num * 50) / 50).toFixed(2);
+  }
+  cart.itemsPrice = addDecimals(
+    cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  );
+  cart.shippingPrice = addDecimals(cart.itemsPrice > 50 ? 0 : 50);
+  cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)));
+  cart.totalPrice = (
+    Number(cart.itemsPrice) +
+    Number(cart.shippingPrice) +
+    Number(cart.taxPrice)
+  ).toFixed(2);
 
   const placeOrderHandler = (e) => {
     e.preventDefault();
@@ -25,8 +47,8 @@ const PlaceOrderScreen = () => {
                 <h5>
                   <strong>Customer</strong>
                 </h5>
-                <p>Admin Random</p>
-                <p>admin@random.com</p>
+                <p>{userInfo.name}</p>
+                <p>{userInfo.email}</p>
               </div>
             </div>
           </div>
@@ -42,8 +64,8 @@ const PlaceOrderScreen = () => {
                 <h5>
                   <strong>Order info</strong>
                 </h5>
-                <p>Shipping: Argentina</p>
-                <p>Pay method: Paypal</p>
+                <p>Shipping: {cart.shippingAddress.country}</p>
+                <p>Pay method: {cart.paymentMethod}</p>
               </div>
             </div>
           </div>
@@ -60,7 +82,9 @@ const PlaceOrderScreen = () => {
                   <strong>Deliver to</strong>
                 </h5>
                 <p>
-                  Address: Argentina,CABA, 1234 Random Address
+                  Address: {cart.shippingAddress.city}, {" "}
+                   {cart.shippingAddress.address}, {" "}
+                   {cart.shippingAddress.postalCode}
                 </p>
               </div>
             </div>
@@ -69,26 +93,36 @@ const PlaceOrderScreen = () => {
 
         <div className="row order-products justify-content-between">
           <div className="col-lg-8">
-            {/* <Message variant="alert-info mt-5">Your cart is empty</Message> */}
-
-            <div className="order-product row">
+            {
+              cart.cartItems.length === 0 ? (
+                <Message variant="alert-info mt-5">Your cart is empty</Message>
+              )
+              :
+              (
+                <>
+                {
+                  cart.cartItems.map((item,index) => (
+                     <div className="order-product row" key={index}>
               <div className="col-md-3 col-6">
-                <img src="/images/8.png" alt="product" />
+                <img src={item.image} alt={item.name} />
               </div>
               <div className="col-md-5 col-6 d-flex align-items-center">
-                <Link to={"/"}>
-                  <h6>CAJA DE DESAYUNO 1</h6>
+                <Link to={`/products/${item.product}`}>
+                  <h6>{item.name}</h6>
                 </Link>
               </div>
               <div className="mt-3 mt-md-0 col-md-2 col-6  d-flex align-items-center flex-column justify-content-center ">
                 <h4>QUANTITY</h4>
-                <h6>4</h6>
+                <h6>{item.qty}</h6>
               </div>
               <div className="mt-3 mt-md-0 col-md-2 col-6 align-items-end  d-flex flex-column justify-content-center ">
                 <h4>SUBTOTAL</h4>
-                <h6>$30</h6>
+                <h6>${item.qty * item.price}</h6>
               </div>
             </div>
+                  ))}
+                </>
+              )}                 
           </div>
           {/* total */}
           <div className="col-lg-3 d-flex align-items-end flex-column mt-5 subtotal-order">
@@ -98,33 +132,38 @@ const PlaceOrderScreen = () => {
                   <td>
                     <strong>Products</strong>
                   </td>
-                  <td>$345</td>
+                  <td>${cart.itemsPrice}</td>
                 </tr>
                 <tr>
                   <td>
                     <strong>Shipping</strong>
                   </td>
-                  <td>$123</td>
+                  <td>${cart.shippingPrice}</td>
                 </tr>
                 <tr>
                   <td>
                     <strong>Tax</strong>
                   </td>
-                  <td>$5</td>
+                  <td>${cart.taxPrice}</td>
                 </tr>
                 <tr>
                   <td>
                     <strong>Total</strong>
                   </td>
-                  <td>$5678</td>
+                  <td>${cart.totalPrice}</td>
                 </tr>
               </tbody>
             </table>
+            {
+              cart.cartItems.length === 0 ? null : (
             <button type="submit" onClick={placeOrderHandler}>
-              <Link to="/order" className="text-white">
+             
                 PLACE ORDER
-              </Link>
+              
             </button>
+              )
+            }
+            
             {/* <div className="my-3 col-12">
                 <Message variant="alert-danger">{error}</Message>
               </div> */}
