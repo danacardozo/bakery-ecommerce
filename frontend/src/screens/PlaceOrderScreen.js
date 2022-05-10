bcf3c2e47714735f7c71348bf9ff31043a1d07d9
danacardozo <dana.n.cardozo@gmail.com>
2022-05-10 13:05:13 -0300

    order screen

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -86,8 +86,8 @@ const PlaceOrderScreen = ({history}) => {
                 <h5>
                   <strong>Order info</strong>
                 </h5>
-                <p>Shipping: {cart.shippingAddress.country}</p>
-                <p>Pay method: {cart.paymentMethod}</p>
+                <p>Shipping:{cart.shippingAddress.country}</p>
+                <p>Pay method:{cart.paymentMethod}</p>
               </div>
             </div>
           </div>
