0e9d0bb00596f0151587b883a2469c5727c1e009
danacardozo <dana.n.cardozo@gmail.com>
2022-05-11 15:14:42 -0300

    review api

--- server/Routes/ProductRoutes.js
+++ server/Routes/ProductRoutes.js
@@ -26,4 +26,43 @@ ProductRoutes.get(
     })
 );
 
+//opiniones de producto 
+ProductRoutes.post(
+    "/:id/review",
+     asyncHandler(async(req,res)=>{
+         const {rating, comment} = req.body
+        const product = await Product.findById(req.params.id);
+
+        if(product){
+            const alreadyReviewed = product.reviews.find(
+                (r) => r.user.toString() === req.user._id.toString()
+            )
+            if (alreadyReviewed) {
+                res.status(400);
+                throw new Error("Product already Reviewed");
+            }
+            const review = {
+                name:req.user.name,
+                rating:Number(rating),
+                comment,
+                user:req.user._id,
+            };
+
+            product.reviews.push(review)
+            product.numReviews = product.reviews.length
+            product.rating =
+            product.reviews.reduce((acc,item) => item.rating + acc, 0) /
+            product.reviews.length;
+
+            await product.save()
+            res.status(201).json({message:"Reviewed Added"})
+        } else {
+             res.status(404);
+             throw new Error("Product not found");
+        }
+        
+    })
+);
+
+
 export default ProductRoutes;
