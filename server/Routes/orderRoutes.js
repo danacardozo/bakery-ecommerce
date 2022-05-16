c14e8f0673d33e4457d62ac89aa67425376515d8
danacardozo <dana.n.cardozo@gmail.com>
2022-05-16 18:32:52 -0300

    show user order misspelled fixed

--- server/Routes/orderRoutes.js
+++ server/Routes/orderRoutes.js
@@ -47,7 +47,7 @@ orderRouter.get(
     "/", 
     protect,
     asyncHandler(async(req,res) => {
-        const order = await Order.find({user: req.user_.id}).sort({_id: -1});
+        const order = await Order.find({user: req.user._id}).sort({_id: -1});
 
    res.json(order);
     })
